cb6947b79d9c480a343bc3aa252cd741f370bf8d
SadFarzan <145829991+SadFarzan@users.noreply.github.com>
2023-10-28 14:18:36 +0700

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1698474470,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/download-sheet0.png",
+		"images/pngtreestraightroadpngimage_-sheet0.png",
+		"images/pngtreestraightroadpngimage_6262171removebg-sheet0.png",
+		"images/3112png_860removebgpreview-sheet0.png",
+		"images/elementstone-sheet0.png",
+		"images/pngtreefinishlineblackandwhitecheckeredseamlessvectortransparentpngimage_-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
